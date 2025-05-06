8a73df55daf6897e7a59a1263188155b1e10127f
Rebecca-TONDE-T <tonderebecca113@gmail.com>
2025-05-07 00:05:03 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const target
+  ::contentReference[oaicite:0]{index=0}
+   
+  
